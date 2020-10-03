aaa667b0835a194fb1338c8eec66cdfb4c228e2b
Alex Frank <alexfrankcodes@gmail.com>
2020-10-02 20:15:26 -0700

    Added basic unit tests

---
+++ src/index.test.js
@@ -0,0 +1,16 @@
+const expect = require("chai").expect;
+const randomEmoji = require("./index");
+const emojiJSON = require("./emojis.json");
+
+describe("all", () => {
+  it("Should return an array of all emojis", () => {
+    expect(randomEmoji.all).to.eql(emojiJSON);
+  });
+});
+
+describe("random", () => {
+  it("Should return a single random emoji", () => {
+    const selectedEmoji = randomEmoji.random();
+    expect(emojiJSON).to.include(selectedEmoji);
+  });
+});
